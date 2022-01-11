bed2df7719f60c3dc553d15068ee18e79b047bed
KENNETH PHANG <bunnyppl@hotmail.com>
2022-01-11 14:27:26 +0800

    upd

--- src/main/java/sg/edu/nus/workshop14/service/RedisRepo.java
+++ src/main/java/sg/edu/nus/workshop14/service/RedisRepo.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface RedisRepo {
+public interface RedisRepo  {
         public void save(final Contact ctc);
         public Contact findById(final String contactId);
         public List<Contact> findAll(int startIndex);
